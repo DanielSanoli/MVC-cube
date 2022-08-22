e2a026ff3f98b9f9c72fdf4b2eb98a433dc2525d
DanielSanoli <daniel.sanoli28@gmail.com>
2022-08-22 14:46:12 -0300

    Implementação de spring-security para fazer o controle de acesso com uso de login

---
+++ src/main/java/br/com/metasix/mvcube/security/ImplementsUserDetailsService.java
@@ -0,0 +1,32 @@
+package br.com.metasix.mvcube.security;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.core.userdetails.UsernameNotFoundException;
+import org.springframework.stereotype.Service;
+
+import br.com.metasix.mvcube.entity.Usuario;
+import br.com.metasix.mvcube.repository.UsuarioRepository;
+
+@Service
+public class ImplementsUserDetailsService implements UserDetailsService{
+
+	@Autowired
+	UsuarioRepository usuarioRepository;
+	
+	@Override
+	public UserDetails loadUserByUsername(String login) throws UsernameNotFoundException {
+		
+		Usuario existsUsuario = usuarioRepository.findByEmail(login);
+
+		if (existsUsuario == null) {
+			throw new Error("Usuário não existe!");
+		}
+		
+		return UsuarioDetails.create(existsUsuario);
+	}
+	
+	
+
+}
---
+++ src/main/java/br/com/metasix/mvcube/security/UsuarioDetails.java
@@ -0,0 +1,71 @@
+package br.com.metasix.mvcube.security;
+
+import java.util.ArrayList;
+import java.util.Collection;
+import java.util.List;
+import java.util.stream.Collectors;
+
+import org.springframework.security.core.GrantedAuthority;
+import org.springframework.security.core.authority.SimpleGrantedAuthority;
+import org.springframework.security.core.userdetails.UserDetails;
+
+import br.com.metasix.mvcube.entity.Usuario;
+
+public class UsuarioDetails implements UserDetails{
+
+	private String email;
+	private String senha;
+	private Collection<? extends GrantedAuthority> authorities;
+	
+	public UsuarioDetails(Usuario usuario) {
+		this.email = usuario.getEmail();
+		this.senha = usuario.getSenha();
+		List<SimpleGrantedAuthority> authorities = new ArrayList<>();
+		
+		authorities = usuario.getRoles().stream().map(role ->{
+			return new SimpleGrantedAuthority("ROLE_".concat(role.getNome()));
+		}).collect(Collectors.toList());
+		
+		this.authorities = authorities;
+	}
+	
+	@Override
+	public Collection<? extends GrantedAuthority> getAuthorities() {
+		return authorities;
+	}
+	
+	public static UsuarioDetails create(Usuario usuario) {
+		return new UsuarioDetails(usuario);
+	}
+
+	@Override
+	public String getPassword() {
+		return senha;
+	}
+
+	@Override
+	public String getUsername() {
+		return email;
+	}
+
+	@Override
+	public boolean isAccountNonExpired() {
+		return true;
+	}
+
+	@Override
+	public boolean isAccountNonLocked() {
+		return true;
+	}
+
+	@Override
+	public boolean isCredentialsNonExpired() {
+		return true;
+	}
+
+	@Override
+	public boolean isEnabled() {
+		return true;
+	}
+
+}
