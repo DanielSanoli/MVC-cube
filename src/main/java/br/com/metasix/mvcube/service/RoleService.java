b86c535521b25acf34d94f3609f04d2936c84743
DanielSanoli <daniel.sanoli28@gmail.com>
2022-08-25 14:38:24 -0300

    criando método que deixa a role setado no cadastro de cidadão

--- src/main/java/br/com/metasix/mvcube/service/RoleService.java
+++ src/main/java/br/com/metasix/mvcube/service/RoleService.java
@@ -1,17 +1,20 @@
 package br.com.metasix.mvcube.service;
 
 import java.util.ArrayList;
+import java.util.Arrays;
 import java.util.List;
 import java.util.Optional;
 import java.util.stream.Collectors;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
 
 import br.com.metasix.mvcube.dto.RoleDTO;
 import br.com.metasix.mvcube.entity.Usuario;
 import br.com.metasix.mvcube.entity.Role;
 import br.com.metasix.mvcube.repository.UsuarioRepository;
 
+@Service
 public class RoleService {
 
 	@Autowired
@@ -32,6 +35,32 @@ public class RoleService {
 
 		Usuario usuario = usuarioExists.get();
 
+		usuario.setRoles(roles);
+
+		UsuarioRepository.save(usuario);
+
+		return usuario;
+
+	}
+	
+	public Usuario executeCidadao(RoleDTO roleDTO) {
+
+		Optional<Usuario> usuarioExists = UsuarioRepository.findById(roleDTO.getIdUsuario());
+		List<Role> roles = new ArrayList<>();
+
+		Long idRole = 2L;
+		roleDTO.setIdsRoles(Arrays.asList(idRole));
+		
+		if (usuarioExists.isEmpty()) {
+			throw new Error("Usuário não existe!");
+		}
+		
+		roles = roleDTO.getIdsRoles().stream().map(role -> {
+			return new Role(role);
+		}).collect(Collectors.toList());
+		
+		Usuario usuario = usuarioExists.get();
+		
 		usuario.setRoles(roles);
 
 		UsuarioRepository.save(usuario);
