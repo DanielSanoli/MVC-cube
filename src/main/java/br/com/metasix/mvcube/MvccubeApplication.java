f11afb0632f9a19605d48c5f4c50ba139b366f67
DanielSanoli <daniel.sanoli28@gmail.com>
2022-08-26 11:13:08 -0300

    Tirando senha criptografada

--- src/main/java/br/com/metasix/mvcube/MvccubeApplication.java
+++ src/main/java/br/com/metasix/mvcube/MvccubeApplication.java
@@ -2,16 +2,12 @@ package br.com.metasix.mvcube;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 
 @SpringBootApplication
 public class MvccubeApplication {
 
 	public static void main(String[] args) {
 		SpringApplication.run(MvccubeApplication.class, args);
-		
-		String encode = new BCryptPasswordEncoder().encode("1234");
-		System.out.println(encode);
 	}
 
 }
