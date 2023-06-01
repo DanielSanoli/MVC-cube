9dc356d5ad8340acb8fd38de8b7c722b520d87ba
DanielSanoli <daniel.sanoli28@gmail.com>
2023-06-01 15:55:22 -0300

    criando busca de todos os chamados ordenados por id

--- src/main/java/br/com/metasix/mvcube/repository/ChamadoRepository.java
+++ src/main/java/br/com/metasix/mvcube/repository/ChamadoRepository.java
@@ -1,6 +1,9 @@
 package br.com.metasix.mvcube.repository;
 
+import java.util.List;
+
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
 import org.springframework.stereotype.Repository;
 
 import br.com.metasix.mvcube.entity.Chamado;
@@ -8,4 +11,7 @@ import br.com.metasix.mvcube.entity.Chamado;
 @Repository
 public interface ChamadoRepository extends JpaRepository<Chamado, Long>{
 
+	@Query("SELECT u FROM Chamado u ORDER BY id")
+	List<Chamado> findAllOrderById();
+	
 }
