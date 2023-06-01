02c15b543226d8e02f4204b4a8ff27df52dc4bb9
DanielSanoli <daniel.sanoli28@gmail.com>
2023-06-01 15:18:42 -0300

    mudando tipo do atributo TipoChamado

--- src/main/java/br/com/metasix/mvcube/dto/ChamadoDTO.java
+++ src/main/java/br/com/metasix/mvcube/dto/ChamadoDTO.java
@@ -1,9 +1,11 @@
 package br.com.metasix.mvcube.dto;
 
 import javax.validation.constraints.NotBlank;
+import javax.validation.constraints.NotNull;
 
 import br.com.metasix.mvcube.entity.Chamado;
 import br.com.metasix.mvcube.entity.Usuario;
+import br.com.metasix.mvcube.enums.TipoChamado;
 
 public class ChamadoDTO {
 
@@ -11,6 +13,8 @@ public class ChamadoDTO {
 	private String titulo;
 	@NotBlank
 	private String descricao;
+	@NotNull
+	private TipoChamado tipoChamado;
 	
 	private Long idUsuario;
 
@@ -37,12 +41,21 @@ public class ChamadoDTO {
 	public void setIdUsuario(Long idUsuario) {
 		this.idUsuario = idUsuario;
 	}
+	
+	public TipoChamado getTipoChamado() {
+		return tipoChamado;
+	}
+
+	public void setTipoChamado(TipoChamado tipoChamado) {
+		this.tipoChamado = tipoChamado;
+	}
 
 	public Chamado toChamado(Usuario usuario) {
 		Chamado chamado = new Chamado();
 		chamado.setTitulo(titulo);
 		chamado.setDescricao(descricao);
 		chamado.setUsuario(usuario);
+		chamado.setTipoChamado(tipoChamado);
 		return chamado;
 	}
 
