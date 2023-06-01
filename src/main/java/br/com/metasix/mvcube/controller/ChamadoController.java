02b1c68c6f3b111f25efa4a1298114a1ca8f7b27
DanielSanoli <daniel.sanoli28@gmail.com>
2023-06-01 15:18:12 -0300

    mudança do modelo/get

--- src/main/java/br/com/metasix/mvcube/controller/ChamadoController.java
+++ src/main/java/br/com/metasix/mvcube/controller/ChamadoController.java
@@ -8,6 +8,7 @@ import org.springframework.security.core.Authentication;
 import org.springframework.security.core.context.SecurityContextHolder;
 import org.springframework.security.core.userdetails.UserDetails;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.validation.BindingResult;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
@@ -30,8 +31,9 @@ public class ChamadoController {
 	UsuarioService usuarioService;
 
 	@GetMapping("formChamado")
-	public String formChamado(ChamadoDTO chamado) {
-		return "chamado/cadastroChamado";
+	public String formChamado(Model model) {
+	    model.addAttribute("chamadoDTO", new ChamadoDTO());
+	    return "chamado/cadastroChamado";
 	}
 	
 	@PostMapping("/formChamado")
